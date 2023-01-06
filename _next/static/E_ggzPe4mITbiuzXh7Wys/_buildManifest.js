bfff439d25d3177497866e90369112636c355e17
NekoZ-Meow <okc5424938d474w@gmail.com>
2023-01-07 00:08:35 +0900

    アプリケーション追加

---
+++ _next/static/E_ggzPe4mITbiuzXh7Wys/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/380-bbb532be6c8fa3ff.js","static/chunks/pages/index-8637db2e7aa8efd5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
